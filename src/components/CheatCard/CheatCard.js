86e7d377a807461051424e3f4f1e050cc824e969
talha-pro <55407333+talha-pro@users.noreply.github.com>
2019-12-27 14:28:17 +0500

    Cards Padding Fixed

--- src/components/CheatCard/CheatCard.js
+++ src/components/CheatCard/CheatCard.js
@@ -7,9 +7,10 @@ import { coy } from 'react-syntax-highlighter/dist/esm/styles/prism'
 const CheatCardContainer = styled.div`
 display:flex;
 flex-flow:column wrap;
-width: 600px;
-margin-top:10px;
+width: 400px;
 margin-bottom:30px;
+justify-content:flex-start;
+padding: 0px;
 `
 const HeadingLineWrapper = styled.div`
 display:flex;
@@ -27,17 +28,16 @@ flex: 15 1 auto;
 const HeadingLine = styled.hr`
 background-image: linear-gradient(to left, rgba(0,255,0,0), rgba(0,255,0,1));
 height: 1px;
-margin-top: 20px;
+margin-top: 15px;
 `
-const CodeDiv = styled.div`
-flex: 0 1 auto;
-box-shadow: 0 0 20px rgba(105, 105, 105, .3), 0 0px 40px rgba(105, 255, 255, .1);
-`
+
 const CodeWrapper = styled.pre`
 position: relative;
 flex: 0 1 auto;
 background-color:white;
 transition: all 0.5s ease 0s;
+box-shadow: 0 0 20px rgba(105, 105, 105, .3), 0 0px 40px rgba(105, 255, 255, .1);
+padding-bottom:0px;
 
 :hover {
  letter-spacing: 0.5px;
@@ -56,6 +56,8 @@ transition: all 0.5s ease 0s;
 `
 const HighLighter = styled.div`
 width: 100px;
+flex: 0 1 auto;
+padding:0px;
 `
 const CheatCard = ({ cardTitle, cardText }) => {
   return (
@@ -71,16 +73,15 @@ const CheatCard = ({ cardTitle, cardText }) => {
             <HeadingLine />
           </HeadingLineDiv>
         </HeadingLineWrapper>
-        <CodeDiv>
-          <CodeWrapper>
-            <CopyButton className="Button" copyText={cardText}></CopyButton>
-            <HighLighter>
-              <SyntaxHighlighter language="javascript" style={coy} >
-                {cardText}
-              </SyntaxHighlighter>
-            </HighLighter>
-          </CodeWrapper>
-        </CodeDiv>
+        <CodeWrapper>
+          <CopyButton className="Button" copyText={cardText}></CopyButton>
+          <HighLighter>
+            <SyntaxHighlighter language="javascript" style={coy} >
+              {cardText}
+            </SyntaxHighlighter>
+          </HighLighter>
+        </CodeWrapper>
+
       </CheatCardContainer>
     </>
   )
